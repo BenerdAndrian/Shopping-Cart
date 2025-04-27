415601fccad2362887ead79b274c072ea1346b87
BenerdAndrian <ben527466@gmail.com>
2025-04-27 07:00:49 +0000

    create custom hook useFetchAPI to fetch data,create ProductPage component which use custom hook to fetch and use the data to render the UI listing all products out styling with tailwind,add find function based on the input using useEffect to filter the array

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -1,6 +1,78 @@
+import { useState,useEffect } from "react"
+const useFetchAPI=()=>{
+    const [dataList,setDataList]=useState({})
+    const [error,setError]=useState(false);
+    const [loading,setLoading]=useState(true)
+    useEffect(()=>{
+        fetch('https://dummyjson.com/products?limit=30')
+        .then((response)=>{
+            if(!response.ok){
+                throw new Error(`The server is Error: ${response.status}`)
+            }
+            return response.json()
+        })
+        .then((data)=>{
+            setDataList(data)
+        })
+        .catch((err)=>{
+            setError(err)
+        })
+        .finally(()=>{setLoading(false)})
+    },[])
+    return {dataList,error,loading}
+}
+
+
 function ProductPage(){
+    const {dataList,error,loading}=useFetchAPI();
+    const [productName,setProductName]=useState('');
+    const [filteredArray,setFilteredArray]=useState([])
+    const findProduct=(e)=>{
+      setProductName(e.target.value)
+    };
+
+    useEffect(()=>{
+        if(dataList.products){
+            const filterArray=dataList.products.filter((item)=>{
+            return item.title.toLowerCase().includes(productName.toLowerCase())
+        })
+        setFilteredArray(filterArray)
+    
+      
+      setFilteredArray(filterArray)
+    }
+    },[dataList,productName])
+    if(error) return <p>There are some Errors went on.</p>
+    if(loading) return <p>Loading...</p>
     return(
-        <h1 className="text-[2rem] font-bold text-center pt-5">This is the product page</h1>
+        <>
+           <h1 className="text-[2rem] font-bold text-center pt-5">This is the product page</h1>
+           <div className="p-3">
+           <input onChange={findProduct} type="text" placeholder="Search for items" className="text-[1.3rem] p-1 w-full border" />
+           <ul className="flex flex-wrap gap-5 justify-center pt-5">
+            {
+            (productName==='' || filteredArray.length>0)?
+            (filteredArray.length>0?filteredArray:dataList.products).map((item)=>(
+                <li className="border rounded-2xl p-4 w-65 flex flex-col">
+                    <img className="w-50 h-50" src={item.thumbnail} alt="product's image" />
+                    <h2 className="font-bold text-[1.3rem]">{item.title}</h2>
+                    <h3>Price: {item.price}$</h3>
+                    <p className="text-gray-400 text-[0.8rem]">{item.description}</p>
+                    <div className="pt-3 mt-auto flex flex-row items-center justify-evenly">
+                    <button className="text-white bg-orange-700 rounded-full py-0.5 px-1.5 cursor-pointer">Add To Cart</button>
+                    <button className="text-white bg-emerald-500 rounded-full py-o.5 px-1.5 cursor-pointer">More...</button>
+                    </div>
+                  
+                </li>
+            ))
+            :
+            <p>There is no data.</p>
+        }
+           </ul>
+           </div>
+          
+        </>
+       
     )
 }
 export {ProductPage}
